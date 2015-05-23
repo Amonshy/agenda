5c7f835c18e66298395faecda2e2e8a3fba9250c
Amonshy <amonshhy@gmail.com>
2015-05-23 22:48:29 +0200

    Clase auxiliar para introducir la fecha de nacimiento del paciente y para el sistema de citas

---
+++ fecha.hpp
@@ -0,0 +1,25 @@
+/*!
+  \file   fecha.hpp
+  \brief  Definición de la la clase Fecha
+  \author Fernando Aguilera Madueño
+  \date   23/05/2015
+*/
+
+#ifndef _FECHA_HPP_
+#define _FECHA_HPP_
+
+//!  Definición de la clase Fecha
+class Fecha 
+{
+  //! \name Atributos privados de la clase Paciente
+private:
+  int _dia;	//!< Día de la fecha introducida
+  int _mes;	//!< Més de la fecha introducida
+  int _year;	//!< Año de la fecha introducida
+  
+public:
+  
+  
+};
+
+#endif
