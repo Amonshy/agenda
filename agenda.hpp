ffc4f8322bffece29bef2267a9f071753ff93a50
Amonshy <amonshhy@gmail.com>
2015-05-24 12:55:03 +0200

    Añadida la clase Agenda:  + Se han añadido modificadores y observadores.  + En principio, se ha considerado el modificador setBaseDeDatos como protected, aunque esto debe ser reevaluado en una fase posterior del proyecto.

---
+++ agenda.hpp
@@ -0,0 +1,39 @@
+/*!
+  \file   agenda.hpp
+  \brief  Definición de la la clase Agenda
+  \author Fernando Aguilera Madueño
+  \date   24/05/2015
+*/
+
+#ifndef _AGENDA_HPP_
+#define _AGENDA_HPP_
+
+#include "paciente.hpp"
+
+//!  Definición de la clase Agenda
+class Agenda
+{
+  //! \name Atributos privados de la clase Agenda
+private:
+  std::string _baseDeDatos;
+  
+public:
+  //Observers
+  //! \name Funciones de consulta de Agenda
+  
+  inline std::string getBaseDeDatos()const
+  {
+    return (_baseDeDatos);
+  }
+  
+protected:
+  
+  //Modifiers
+  //! \name Funciones de modificación de Agenda
+  inline void setBaseDeDatos(const std::string & nuevaBaseDeDatos)
+  {
+    _baseDeDatos=nuevaBaseDeDatos;
+  }
+};
+
+#endif
