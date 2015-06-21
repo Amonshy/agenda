0abd19f88846ce0f6a0c5539b3a9d1a45748d19b
Amonshy <amonshhy@gmail.com>
2015-06-21 13:08:02 +0200

    Arreglo de la función de almacenaje en la base de datos

--- paciente.cpp
+++ paciente.cpp
@@ -8,6 +8,7 @@ Paciente Paciente::rellenarDatosPaciente()
 
   int posicionBarra, posicionBarra2;
   std::string aux, aux2;
+  std::string cita;
   
   getchar();
   //-----------------------------------------------------------------------------------------------------------
@@ -49,7 +50,7 @@ Paciente Paciente::rellenarDatosPaciente()
     p1._fechaNacimiento.setDia(std::stoi(aux2));
   }
   
-  //Obtenemos la posicón de la segunda barra
+  //Obtenemos la posición de la segunda barra
   posicionBarra2=aux.find_first_of("/",posicionBarra+1);
   aux2= aux.substr(posicionBarra+1,posicionBarra2);
 
@@ -79,34 +80,60 @@ Paciente Paciente::rellenarDatosPaciente()
   //CITA DEL PACIENTE
   LUGAR(10,10);
   std::cout << "Introduzca la cita del paciente (DD/MM/YYYY): ";
-  std::cin >> aux;
+  std::cin >> cita;
   
   //Obtenemos la posicón de la primera barra
-  posicionBarra=aux.find_first_of("/");
-  aux2= aux.substr(0,posicionBarra);
+  posicionBarra=cita.find_first_of("/");
+  aux2= cita.substr(0,posicionBarra);
   
   if(std::stoi(aux2)) //La función stoi requiere del standard c++11
   {
-    p1._citaPaciente.getFechaCita().setDia(std::stoi(aux2));
+    p1._citaPaciente.setDiaFechaCita(std::stoi(aux2));
   }
   
-  //Obtenemos la posicón de la segunda barra
-  posicionBarra2=aux.find_first_of("/",posicionBarra+1);
-  aux2= aux.substr(posicionBarra+1,posicionBarra2);
+  //Obtenemos la posición de la segunda barra
+  posicionBarra2=cita.find_first_of("/",posicionBarra+1);
+  aux2= cita.substr(posicionBarra+1,posicionBarra2);
 
   if(std::stoi(aux2)) //La función stoi requiere del standard c++11
   {
-    p1._citaPaciente.getFechaCita().setMes(std::stoi(aux2));
+    p1._citaPaciente.setMesFechaCita(std::stoi(aux2));
   }
   
   //Obtenemos el año
-  aux2=aux.substr(posicionBarra2+1);
+  aux2=cita.substr(posicionBarra2+1);
   
   if(std::stoi(aux2)) //La función stoi requiere del standard c++11
   {
-    p1._citaPaciente.getFechaCita().setYear(std::stoi(aux2));
+    p1._citaPaciente.setYearFechaCita(std::stoi(aux2));
   }
   
+  getchar();
+  LUGAR(11,10);
+  std::cout << "Introduzca el motivo de la cita: ";
+  std::getline(std::cin, aux);
+  p1._citaPaciente.setMotivo(aux); 
+  
+  LUGAR(12,10);
+  std::cout << "Introduzca la hora de la cita (HH:MM): ";
+  std::cin >> aux;
+  
+  //Obtenemos la posicón de los :
+  posicionBarra=aux.find_first_of(":");
+  aux2= aux.substr(0,posicionBarra);
+   
+  if(std::stoi(aux2)) //La función stoi requiere del standard c++11
+  {
+    p1._citaPaciente.setHoraCita(std::stoi(aux2));
+  }
+  
+  aux2=aux.substr(posicionBarra+1);
+  
+  if(std::stoi(aux2)) //La función stoi requiere del standard c++11
+  {
+    p1._citaPaciente.setMinutosCita(std::stoi(aux2));
+  }
+   
   //-----------------------------------------------------------------------------------------------------------
 
   
