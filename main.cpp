c656863fce48c005598c895b57c686399cd23751
Amonshy <amonshhy@gmail.com>
2015-06-22 13:16:07 +0200

    Añadidas mejoras para la visualización

--- main.cpp
+++ main.cpp
@@ -19,7 +19,8 @@ int main(int argc, char **argv)
   int opcion;
   Paciente p1;
   Agenda agendaDentista;
-  std::string nombre;
+  std::string nombre, dni;
+  int borrar;
    
   do{
     BORRAR;
@@ -36,9 +37,7 @@ int main(int argc, char **argv)
     LUGAR(10,10);
     std::cout << "5. Listar agenda" << std::endl;
     LUGAR(11,10);
-    std::cout << "6. Copia de seguridad" << std::endl;
-    LUGAR(12,10);
-    std::cout << "7. Salir" << std::endl;
+    std::cout << "6. Salir" << std::endl;
 
     LUGAR(15,10);
     std::cout << "Opción: ";
@@ -47,7 +46,26 @@ int main(int argc, char **argv)
     switch(opcion)
     {  
       case 1:
-	
+	BORRAR;
+	LUGAR(5,10);
+	std::cout << "Introduzca el nombre del paciente a buscar: ";
+	std::cin >> nombre;
+	LUGAR(10,10);
+	std::cout << "Para la correcta visualización, abra su terminal en modo pantalla completa";
+	APAGA;
+	LUGAR(23,25);
+	std::cout << "pulse ";
+	SUBRAYADO;
+	std::cout << "ENTER";
+	APAGA;
+	std::cout << " para ";
+	INVERSO;
+	std::cout << "continuar"; 
+	APAGA;
+	getchar();
+	getchar();
+	BORRAR;
+	agendaDentista.buscarPacienteEnBaseDeDatos(nombre);
 	break;
 	
       case 2:
@@ -57,7 +75,10 @@ int main(int argc, char **argv)
 	break;
 	
       case 3:
-	
+	BORRAR;
+	LUGAR(5,10);
+	std::cout << "Introduzca el dni del paciente a modificar: ";
+	std::cin >> dni;
 	break;
       
       case 4:
@@ -65,19 +86,40 @@ int main(int argc, char **argv)
 	LUGAR(5,10);
 	std::cout << "Introduzca el nombre del paciente a eliminar: ";
 	std::cin >> nombre;
-	agendaDentista.eliminarDeBaseDeDatos(nombre);
+	borrar=agendaDentista.eliminarDeBaseDeDatos(nombre);
+	if(borrar>0)
+	{
+	  LUGAR(7,10);
+	  std::cout << "Se han eliminado " << borrar << " coincidencias";
+	  
+	}
+	else
+	{
+	  LUGAR(7,10);
+	  std::cout << "No se ha encontrado ninguna coincidencia";
+	}
 	break;
 	
       case 5:
+	BORRAR;
+	LUGAR(10,10);
+	std::cout << "Para la correcta visualización, abra su terminal en modo pantalla completa";
+	APAGA;
+	LUGAR(23,25);
+	std::cout << "pulse ";
+	SUBRAYADO;
+	std::cout << "ENTER";
+	APAGA;
+	std::cout << " para ";
+	INVERSO;
+	std::cout << "continuar"; 
+	APAGA;
+	getchar();
 	BORRAR;
 	agendaDentista.listarBaseDeDatos();
 	break;
 	
       case 6:
-	
-	break;
-	
-      case 7:
 	BORRAR;
 	LUGAR(5,10);
 	std::cout << "Gracias por usar el programa" << std::endl;
@@ -100,11 +142,18 @@ int main(int argc, char **argv)
     INVERSO;
     std::cout << "continuar"; 
     APAGA;
-    getchar();
-    getchar();
+    if(opcion==1 or opcion==5)
+    {
+      getchar();
+    }
+    else
+    {
+      getchar();
+      getchar();
+    }    
     BORRAR;
     
-  }while(opcion!=7);
+  }while(opcion!=6);
   
   return (0);
 }
